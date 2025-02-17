0d7a9c05e5704a86cbfb5c9348e97ec999326ebf
Floxde99 <florian.fchr@hotmail.com>
2025-02-17 10:59:27 +0100

    final version

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const prompt = require("prompt-sync")()
 let PVJ = 100
 let PVO = 100
+
 let choice = 0
-while (PVJ >= 0 || PVO >= 0) {
+let attacks = [-10,15,-20,-30]
+let min = [1,1,1,1]
+let max = [2,3,3,4]
+console.log("Bienvenue dans le jeux de combat dont on a pas la licence :");
+console.log("Tu vas devoir faire un choix entre 4 attaque et tu dois battre l'ordinateur");
+console.log("Liste des Attaques Disponibles");
+console.log("Attaque 1 : Nom: Frappe Rapide / Puissance: 10 PV/ Précision: 50%");
+console.log("Attaque 2 : Nom: Soin Léger / Puissance: Soigne 15 PV / Précision: 33.33%");
+console.log("Attaque 3 : Nom: Coup Puissant / Puissance: 20 PV / Précision: 33.33% ");
+console.log("Attaque 4 : Nom: Frappe Dévastatrice / Puissance: 30 PV / Précision: 25% ");
+
+
+while (PVJ > 0 && PVO > 0) {
     console.log("Tu as ", PVJ, " PV");
     console.log("Ton adversaire a ", PVO, " PV");
     choice = prompt("quelle attaque veux tu faires ? (si tu as oublierla liste des attaques tape h) ")
@@ -11,17 +24,17 @@ while (PVJ >= 0 || PVO >= 0) {
     }
     switch (choice) {
         case "1":
-            attack1(1)
+            Attack(1,0,attacks,min,max)
             break;
         case "2":
-            attack2(1)
-            continue;
+            Attack(1,1,attacks,min,max)
+            break;
         case "3":
-            attack3(1)
-            continue;
+            Attack(1,2,attacks,min,max)
+            break;
         case "4":
-            attack4(1)
-            continue;
+            Attack(1,3,attacks,min,max)
+            break;
 
         default:
             console.log("Liste des Attaques Disponibles");
@@ -31,64 +44,39 @@ while (PVJ >= 0 || PVO >= 0) {
             console.log("Attaque 4 : Nom: Frappe Dévastatrice / Puissance: 30 PV / Précision: 25% ");
             continue;
     }
-    console.log("tas gagner contre un pantin en bois qui ne bouge pas bravo ... enfin je crois");
+    IA()  
+}
+if (PVJ>0) {
+    console.log("Bravo tu as gagner contre un grille pain");
     
-    break;
+}else{
+    console.log("Sombre merde tu as perdu");
+    
 }
-function attack1(perso) {
-    console.log("tu utilises: Frappe rapide");
-    let prob = randomize(1, 2)
+function Attack(perso,num ,attacks,min,max) {
+    console.log("Attaque : ",num+1);
+    let prob = randomize(min[num], max[num])
     if (prob == 2) {
-        console.log("Touché tu lui à enlevé 10 PV");
+        console.log("cool");
+        
         if (perso == 1) {
-            PVO = PVO - 10
+            if (num != 1) {
+                console.log("Touché tu lui à enlevé",attacks[num], " PV");
+                PVO += attacks[num]
+            } else {
+                console.log("Tu t'es régénéré ",attacks[num], " PV");
+                PVJ += attacks[num]
+            }
+            
         } else {
-            PVJ -= 10
-        }
-    } else {
-        console.log("Raté");
-    }
-
-}
-function attack2(perso) {
-    console.log("tu utilises: Soin léger");
-    let prob = randomize(1, 3)
-    if (prob == 2) {
-        console.log("Tu as régénéré 15PV");
-        if (perso == 1) {
-            PVJ += 15
-        } else {
-            PVO += 15
-        }
-    } else {
-        console.log("Raté");
-    }
-
-}
-function attack3(perso) {
-    console.log("tu utilises: Coup puissant");
-    let prob = randomize(1, 3)
-    if (prob == 2) {
-        console.log("Touché tu lui à enlevé 20 PV");
-        if (perso == 1) {
-            PVO -= 20
-        } else {
-            PVJ -= 20
-        }
-    } else {
-        console.log("Raté");
-    }
-
-}
-function attack4(perso) {
-    console.log("tu utilises: Frappe dévastatrice");
-    let prob = randomize(1, 4)
-    if (prob == 2) {
-        console.log("Touché tu lui à enlevé 30 PV");
-        if (perso == 1) {
-            PVO -= 30
-        } else {
-            PVJ -= 30
+            if (num !=1) {
+                console.log("Touché il t'a enlevé",attacks[num], " PV");
+                PVJ += attacks[num]
+            } else {
+                console.log("Il s'est régénéré ",attacks[num], " PV");
+                PVO += attacks[num]
+            }
+            
         }
     } else {
         console.log("Raté");
@@ -98,6 +86,20 @@ function attack4(perso) {
 function randomize(min, max) {
     return Math.round(Math.random() * (max - min) + min)
 }
-function IA(params) {
-
+function IA() {
+    let val = randomize(1,4)
+    switch (val) {
+        case 1 :
+            Attack(2,0,attacks[0],min[0],max[0])
+            break;
+        case 2 :
+            Attack(2,0,attacks[1],min[1],max[1])
+            break;
+        case 3 :
+            Attack(2,0,attacks[2],min[2],max[2])
+            break;
+        case 4 :
+            Attack(2,0,attacks[3],min[3],max[3])
+            break;
+    }
 }
